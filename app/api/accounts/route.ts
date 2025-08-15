bb051acd13daa19fff7d86ca7fe95a9f16a80d7b
overwerk.werk <overwerk.werk@gmail.com>
2025-08-15 06:50:24 +0200

    Implement Create & Read Account Routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,44 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/errors";
+import { ForbiddenError } from "@/lib/https-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const accounts = await Account.find();
+
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const exisitingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+
+    if (exisitingAccount) throw new ForbiddenError("An acconnt with the same provider already exists");
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json({ success: true, data: newAccount }, { status: 201 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
