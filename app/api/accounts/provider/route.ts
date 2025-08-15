498f0aeb6e96dab457f5b8db5e85894a9d3642fb
overwerk.werk <overwerk.werk@gmail.com>
2025-08-15 07:01:06 +0200

    Implement Account by Provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,33 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/errors";
+import { NotFoundError, ValidationError } from "@/lib/https-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema, UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+import { success } from "zod";
+
+export async function POST(request: Request) {
+    const { providerAccountId } = await request.json();
+
+    try {
+        await dbConnect();
+
+        const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+
+        if(!validatedData.success) throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+        const account = await Account.findOne({ providerAccountId });
+        if(!account) throw new NotFoundError('Account');
+
+        return NextResponse.json({
+            success: true,
+            data: account,
+        },
+        { status: 200} );
+        
+    } catch (error) {
+        return handleError(error, 'api') as APIErrorResponse
+        
+    }
+}
